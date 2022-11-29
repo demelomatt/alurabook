ea986ec371fbf23de40b4de12302d5735da1b396
Matheus Melo <demelosmatheus@outlook.com>
2022-11-29 10:09:47 -0300

    Projeto concluído

---
+++ js/swiper.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.swiper', {
+    spaceBetween: 10,
+    slidesPerView: 3,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+});
